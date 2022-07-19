a7ab73ffb3c8ddaec76afd8706e2e2bf3f736ead
Henrique Souza <henrique.asouza@outlook.com>
2022-07-19 19:38:59 -0300

    chore: add notification patter

---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,31 @@
+export type NotificationErrorProps = {
+  message: string;
+  context: string;
+};
+
+export default class Notification {
+  private errors: NotificationErrorProps[] = [];
+
+  public addError(error: NotificationErrorProps) {
+    this.errors.push(error);
+  }
+
+  public messages(context?: string): string {
+    let message = "";
+
+    this.errors.forEach((error) => {
+      if (error.context === context || !context) {
+        message += `${error.context}: ${error.message},`;
+      }
+    });
+    return message;
+  }
+
+  public hasErrors(): boolean {
+    return this.errors.length > 0;
+  }
+
+  getErrors(): NotificationErrorProps[] {
+    return this.errors;
+  }
+}

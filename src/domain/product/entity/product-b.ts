86a7c256b4c4843419b1c387ca0c997d3dce7f14
Henrique Souza <henrique.asouza@outlook.com>
2022-07-19 22:16:19 -0300

    chore: add notification patter on product

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -6,38 +6,7 @@ export default class ProductB extends Product implements ProductInterface {
     super(id, name, price);
   }
 
-  // get id(): string {
-  //   return super.id;
-  // }
-
-  // get name(): string {
-  //   return super.name;
-  // }
-
   get price(): number {
     return super.price * 2;
   }
-
-  // changeName(name: string): void {
-  //   super.changeName(name);
-  //   this.validate();
-  // }
-
-  // changePrice(price: number): void {
-  //   super.changePrice(price);
-  //   this.validate();
-  // }
-
-  // validate(): boolean {
-  //   if (super.id.length === 0) {
-  //     throw new Error("Id is required");
-  //   }
-  //   if (super.name.length === 0) {
-  //     throw new Error("Name is required");
-  //   }
-  //   if (super.price < 0) {
-  //     throw new Error("Price must be greater than zero");
-  //   }
-  //   return true;
-  // }
 }
--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,22 +1,47 @@
 import Product from "./product";
+import ProductB from "./product-b";
 
 describe("Product unit tests", () => {
-  it("should throw error when id is empty", () => {
+  it("should throw error when id is empty on product A", () => {
     expect(() => {
       const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+    }).toThrowError("Product: Id is required");
   });
 
-  it("should throw error when name is empty", () => {
+  it("should throw error when id is empty on product B", () => {
+    expect(() => {
+      const product = new ProductB("", "Product 1", 100);
+    }).toThrowError("Product: Id is required");
+  });
+
+  it("should throw error when name is empty on product A", () => {
     expect(() => {
       const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+    }).toThrowError("Product: Name is required");
+  });
+
+  it("should throw error when name is empty on product B", () => {
+    expect(() => {
+      const product = new ProductB("123", "", 100);
+    }).toThrowError("Product: Name is required");
+  });
+
+  it("should throw error when name and id are empty on product A", () => {
+    expect(() => {
+      const product = new Product("", "", 100);
+    }).toThrowError("Product: Id is required,Product: Name is required");
+  });
+
+  it("should throw error when name and id are empty on product B", () => {
+    expect(() => {
+      const product = new ProductB("", "", 100);
+    }).toThrowError("Product: Id is required,Product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    }).toThrowError("Product: Price must be greater than zero");
   });
 
   it("should change name", () => {
